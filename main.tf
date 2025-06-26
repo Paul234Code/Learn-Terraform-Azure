34540693ad6b889d81c7a96146f62a683c7f426d
Paul Faye <fayepaul234@gmail.com>
2025-06-26 00:38:56 -0400

    ajuster les noms

--- main.tf
+++ main.tf
@@ -13,16 +13,16 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+  name     = "terraformRG"
   location = "West Europe"
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "storageaccountname"
+  name                     = "storage20278"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
-  account_replication_type = "GRS"
+  account_replication_type = "LRS"
 
   tags = {
     environment = "Developpment"
